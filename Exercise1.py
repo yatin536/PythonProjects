aefa1dd974f01449a4521a1f230fd8355b9e14aa
yatin536 <66869735+yatin536@users.noreply.github.com>
2021-05-19 11:19:14 +0530

    Automated Mail Using Python SMTP module
    In this a quote will BE mailed automatically every weekday from list of quotes

--- Exercise1.py
+++ Exercise1.py
@@ -4,8 +4,8 @@ import random
 with open("C:\\Users\\Dell\\Desktop\\worksta\\Python Practice\\Foldr\\Day32SMTP\\quotes.txt",mode="r") as qoutes:
     qt=qoutes.readlines()
 mg=random.choice(qt)   
-usName="yatin.18bcon268@jecrcu.edu.in"
-passw="tonystarkironman"
+usName="YOUR USERNAME"
+passw="YOUR PASSWORD"
 with st.SMTP("smtp.jecrcu.edu.in",port=587) as obs:
     obs.starttls()
     obs.login(user= usName,password=passw)
